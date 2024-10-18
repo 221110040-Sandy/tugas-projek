cbdaacf46a26d2a63b21085632cff4e97ef02753
Sandy Agre Nicola <Sandy@adaremit.co.id>
2024-10-18 09:56:12 +0700

    Set up Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:tugas_akhir/firebase_options.dart';
 import 'package:tugas_akhir/pages/home_screen.dart';
 import 'package:tugas_akhir/pages/splash_screen.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    FirebaseAnalytics.instance.setAnalyticsCollectionEnabled(true);
+    await FirebaseAnalytics.instance.logBeginCheckout(
+        value: 10.0,
+        currency: 'USD',
+        items: [
+          AnalyticsEventItem(
+              itemName: 'SANDY BOY', itemId: 'xjw73ndnw', price: 10),
+        ],
+        coupon: '10PERCENTOFF');
+  } catch (e) {
+    print("Failed to initialize Firebase: $e");
+  }
   runApp(MyApp());
 }
 
